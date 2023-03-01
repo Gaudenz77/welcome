142594915c8712607c4127e10f0a5b78b41de19e
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-03-01 16:26:34 +0100

    xcomit last maybe tinight

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,62 @@
 <template>
-<img alt="Vue logo" src="./assets/logo.png" style="width:10%;">@
-<!-- <i class="fa-solid fa-at fa-5x" style="color:black;"></i> -->
-<img alt="Vue logo" src="./assets/Opportunity.png">
-<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <!-- <WelcomeToOpportunity msg="Welcome to Opportunity"/> -->
-
-  <div class="container">
-      <div class="row justify-content-evenly">
-        <div><WelcomeToOpportunity msg="Welcome to Opportunity"/></div>
-        <div class="col-12 m-3 infoBoxBs">
-          <span class="m-3 px-2">
-              <li class="list-group-item li-time">{{ currentdate }}19:30 Uhr</li>
-              <li class="list-group-item li-topic">{{ title }}Besuch</li>
-              <li class="list-group-item li-time">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</li>
-              </span></div>
-        <div class="col-12 m-3 infoBoxBs">
-          <span class="m-3 px-2">
-              <li class="list-group-item li-time">{{ currentdate }}19:30 Uhr</li>
-              <li class="list-group-item li-topic">{{ title }}Besuch</li>
-              <li class="list-group-item li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</li>
-              </span></div>
-        <div class="col-12 m-3 infoBoxBs">
-          <span class="m-3 px-2">
-              <li class="list-group-item li-time">{{ currentdate }}19:30 Uhr</li>
-              <li class="list-group-item li-topic">{{ title }}Besuch</li>
-              <li class="list-group-item li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</li>
-              </span></div>
-      </div>
-    </div>
-
-
-
   <div class="container">
     <div class="row">
-      <div class="col">
-        <div  id="titleOriginal"><WelcomeToOpportunity msg="Welcome to Opportunity"/></div>
-            <ul class="infoBox">           
-              <li class="">
-              <span class="li-time">{{ currentdate }}19:30 Uhr</span><br>
-              <span class="li-topic">{{ title }}Besuch</span><br>
-              <span class="li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</span>
-              </li>
-            </ul>
-            <ul class="infoBox">   
-              <li class="">
-              <span class="li-time">{{ currentdate }}19:30 Uhr</span><br>
-              <span class="li-topic">{{ title }}Besuch</span><br>
-              <span class="li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</span>
-            </li>
-          </ul>
+      <div class="col text-center" style="font-size:50px;font-weight:bold;">
+        <img alt="Vue logo" src="./assets/logo.png" style="width:10%; items-align:center;">@
+<!-- <i class="fa-solid fa-at fa-4x" style="color:black;"></i> -->
+        <img alt="Vue logo" src="./assets/Opportunity.png">
+<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+<!-- <WelcomeToOpportunity msg="Welcome to Opportunity"/> -->
       </div>
     </div>
   </div>
 
-    
+  <!-- <div class="container">
+      <div class="row justify-content-evenly">
+        <div><WelcomeToOpportunity msg="Welcome to Opportunity"/></div>
+        <div class="col-12 m-3 infoBoxBs">
+          <span class="m-3">
+              <li class="list-group-item li-time">{{ currentdate }}19:30 Uhr</li>
+              <li class="list-group-item li-topic">{{ title }}Besuch</li>
+              <li class="list-group-item li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</li>
+              </span></div>
+        <div class="col-12 m-3 infoBoxBs">
+          <span class="m-3">
+              <li class="list-group-item li-time">{{ currentdate }}19:30 Uhr</li>
+              <li class="list-group-item li-topic">{{ title }}Besuch</li>
+              <li class="list-group-item li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</li>
+              </span></div>
+        <div class="col-12 m-3 infoBoxBs">
+          <span class="m-3">
+              <li class="list-group-item li-time">{{ currentdate }}19:30 Uhr</li>
+              <li class="list-group-item li-topic">{{ title }}Besuch</li>
+              <li class="list-group-item li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</li>
+              </span></div>
+      </div>
+    </div> -->
+
+    <div class="container">
+      <ul>   
+          <li>
+          <span class=""><WelcomeToOpportunity msg="Welcome to Opportunity"/></span>
+        </li>
+      </ul>
+        <ul class="infoBox">           
+          <li>
+          <span class="li-time">{{ currentdate }}19:30 Uhr</span><br>
+          <span class="li-topic">{{ title }}Besuch</span><br>
+          <span class="li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</span>
+          </li>
+        </ul>
+        <ul class="infoBox">   
+          <li>
+          <span class="li-time">{{ currentdate }}19:30 Uhr</span><br>
+          <span class="li-topic">{{ title }}Besuch</span><br>
+          <span class="li-info">{{ info }}Interessierte können uns gerne für den zweiten Kurs besuchen</span>
+        </li>
+      </ul>
+    </div>
+
   <footer>
     <div class="footer-wrapper">
       <div class="footer-item">
@@ -78,64 +81,49 @@ export default {
   name: 'App',
   components: {
     /* HelloWorld, */
-    WelcomeToOpportunity,
-    
+    WelcomeToOpportunity, 
   }
 }
 </script>
 
 <style>
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
-  font-family: 'Inter', sans-serif;
+  font-family: "Inter", Helvetica, Arial, sans-serif;
+  /* font-family: 'Inter', sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  /* color: #2c3e50;
-  margin-top: 60px; */
 }
 
 
 
 body {
-  margin-bottom:40vh;
-}
-
-#titleOriginal {
-position: absolute;
-left:166px;
-margin-top:-200px;
-
-
-
+ 
+  margin-bottom:25vh;
 }
 
 .infoBoxBs {
   background-color: #0F05A0;
+  padding-left:25px;
 
 }
-
 
 .infoBox {
   width:80%;
   margin: 0 auto;
   margin-top:20px;
-  height:fit-content;
+  width:960px;
+  height:182px;
+  /* height:fit-content;  fit content*/
   background-color: #0F05A0;
-  padding:20px;
+  padding:25px;
   text-decoration:none;
-
 }
-
-
 
 .li-time {
   font-family: 'Inter', sans-serif;
   font-size:28px;
   font-weight:900;
   color:#EB5E00;
-  
-  
 }
 
 .li-topic {
@@ -144,8 +132,6 @@ margin-top:-200px;
   font-weight:900;
   color:#FFBFAB;
   text-decoration:none;
-  
-
 }
 
 .li-info {
@@ -153,8 +139,6 @@ margin-top:-200px;
   font-size:28px;
   font-weight:500;
   color:#FFBFAB;
-  
-
 }
 
 footer {
@@ -163,7 +147,6 @@ footer {
   position:fixed;
   bottom: 0;
   z-index: 1;
-  
 }
 
 .footer-wrapper {
@@ -175,17 +158,16 @@ footer {
 }
 
 .footer-item {
-  flex-basis: calc(33.33% - 20px);
+  flex-basis: calc(33.33% - 10px);
   text-align: center;
 }
 
 .footer-item img {
   max-width: 100%;
+ 
 }
-
 
 ul {
   list-style: none;
 }
-
 </style>
--- src/components/WelcomeToOpportunity.vue
+++ src/components/WelcomeToOpportunity.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="conatiner-fluid">
     <div class="row">
-        <div class="col"><h1>{{ msg }}</h1></div>
-        <p>{{currentDate()}}</p>
+        <div class="titleWelcome">{{ msg }}</div>
+        <h2 class="titleTimeDate">{{currentDate()}}</h2>
     </div>
   </div>
 </template>
@@ -14,22 +14,21 @@ export default {
     msg: String,
    
   },
-
+  
   methods: {
     currentDate() {
       const current = new Date();
-      const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+      const date = `0${current.getDate()}.0${current.getMonth()+1}.${current.getFullYear()}`;
       return date;
     }
   }
- 
 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
+/* h1 {
     font-size: 60px;
     font-weight:bold;
     text-align:left;
@@ -37,8 +36,24 @@ h1 {
 h3 {
   margin: 40px 0 0;
 }
+ */
+.titleWelcome {
+  font-family: 'Inter', sans-serif;
+  font-size:62px;
+  font-weight:900;
+  margin-left:-10px; 
+}
 
 
+.titleTimeDate {
+  font-family: 'Inter', sans-serif;
+  font-size:62px;
+  font-weight:500;
+  color:#9AA7B1;
+  text-decoration:none;
+  margin-left:-10px; 
+}
+ 
 /*  ul {
   list-style-type: none;
   padding: 0;
